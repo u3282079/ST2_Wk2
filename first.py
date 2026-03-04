745c66ea458b96b5b7780d9e8d3e1f9adcced5ba
u3282079 <u3282079@uni.canberra.edu.au>
2026-03-04 11:34:04 +1100

    Add files via upload

---
+++ first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+a = 10
+b = 5
+c = 2
+
+sum_ab = a + b
+product_bc = b * c
+difference_ac = a - c
+
+print("a = ", a)
+print("b = ", b)
+print("c = ", c)
+
+
+
+print("a+b=", sum_ab)
+print("b*c=", product_bc)
+print("a-c=", difference_ac)
